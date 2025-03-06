9f8b4f8fb88ced8c104b7bd774ee1951ee6648d0
m1stakkee <ivankonovalov2007@mail.ru>
2025-03-06 20:36:34 +0300

    Authorization

--- WpfApp2/LoginWindow.xaml.cs
+++ WpfApp2/LoginWindow.xaml.cs
@@ -41,62 +41,63 @@ namespace WpfApp2
                 {
                     sqlConnection.Open();
 
-                    
+
                     string query = @"
-                        SELECT COUNT(*), r.Name 
-                        FROM Users p
-                        JOIN Roles r ON p.RoleId = r.RoleId
-                        WHERE p.Login = @login AND p.Password = @password
-                        GROUP BY r.Name";
+                    SELECT COUNT(*), p.RoleId
+                    FROM Users p
+                    WHERE p.Login = @login AND p.Password = @password
+                    GROUP BY p.RoleId";
                     using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
                     {
-                        
+
                         cmd.Parameters.Add("@login", SqlDbType.VarChar).Value = loginUser;
                         cmd.Parameters.Add("@password", SqlDbType.VarChar).Value = passUser;
 
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            if (reader.Read())
+                        
+
+                            using (SqlDataReader reader = cmd.ExecuteReader())
                             {
-                                int userCount = reader.GetInt32(0);
-                                string roleName = reader.GetString(1);
-
-                                if (userCount > 0)
+                                if (reader.Read())
                                 {
-                                    Hide();
+                                    int userCount = reader.GetInt32(0);
+                                    int roleId = reader.GetInt32(1); 
 
-                                    if (roleName.Equals("Админ", StringComparison.OrdinalIgnoreCase))
+                                    if (userCount > 0)
                                     {
-                                        
-                                        AdminWindow adminWindow = new AdminWindow();
-                                        adminWindow.Show();
-                                        MessageBox.Show("Вы вошли как администратор!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                                        Hide();
 
-                                    }
-                                    else if (roleName.Equals("Пользователь", StringComparison.OrdinalIgnoreCase)) 
-                                    {
-                                        
-                                        MainWindow mainWindow = new MainWindow();
-                                        mainWindow.Show();
-                                        MessageBox.Show("Вы вошли как пользователь!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                                        if (roleId == 1) 
+                                        {
+                                            
+                                            AdminWindow adminWindow = new AdminWindow();
+                                            adminWindow.Show();
+                                            MessageBox.Show("Вы вошли как администратор!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                                        }
+                                        else if (roleId == 2) 
+                                        {
+                                            
+                                            MainWindow mainWindow = new MainWindow();
+                                            mainWindow.Show();
+                                            MessageBox.Show("Вы вошли как пользователь!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                                        }
+                                        else
+                                        {
+                                            MessageBox.Show("Неизвестная роль пользователя: " + roleId, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                        }
                                     }
                                     else
                                     {
-                                        MessageBox.Show("Неизвестная роль пользователя: " + roleName, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                        MessageBox.Show("Неправильный логин или пароль!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
                                     }
                                 }
                                 else
                                 {
-                                    MessageBox.Show("Неправильный логин или пароль!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                    MessageBox.Show("Неправильный логин или пароль!!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
                                 }
-                            }
-                            else
-                            {
-                                MessageBox.Show("Неправильный логин или пароль!!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning); 
                             }
                         }
                     }
-                }
+                
             }
             catch (Exception ex)
             {
